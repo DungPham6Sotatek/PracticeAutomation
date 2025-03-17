c002bc239466989ea353018ae790c84c61b0e002
DungPham6Sotatek <dung.pham6@sotatek.com>
2025-03-17 17:32:03 +0700

    Update latest code

--- ProjectTest/src/main/java/automation/testsuite/LoginTest.java
+++ ProjectTest/src/main/java/automation/testsuite/LoginTest.java
@@ -1,15 +1,24 @@
 package automation.testsuite;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import automation.common.CommonBase;
 
 
 public class LoginTest extends CommonBase{
-	String ebayPrice; 
+	String ebayPrice;
+	
+	@BeforeMethod
+	public void openBrowser() {
+		driver = initBrowser("https://www.ebay.com/");
+	}
+	
+	
 	@Test
 	public void TestO1() {
-		driver = initBrowser("https://www.ebay.com/");
+		
 		WebElement inputSearch = driver.findElement(By.id("gh-ac"));
 		inputSearch.click();
 		inputSearch.sendKeys("iPhone 16");
@@ -18,8 +27,9 @@ public class LoginTest extends CommonBase{
 		WebElement primaryPrice = driver.findElement(By.xpath("//div[@class= 's-item__details-section--primary']//span"));
 		
 		primaryPrice = waitElementVisible(By.xpath("//div[@class= 's-item__details-section--primary']//span"), 20);
-		ebayPrice = primaryPrice.getText();
-		System.out.println(ebayPrice);
+		
+//		ebayPrice = primaryPrice.getText();
+//		System.out.println(ebayPrice);
 		driver.quit();
 	}
 }
